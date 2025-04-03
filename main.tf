51cd7c3a798e19bce0c12d81066f5c4c5bf9462a
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-03 17:17:21 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,38 @@
+resource "aws_iam_user" "aws_terraform_user" {
+  name = "aws_terraform_user"
+}
+
+resource "aws_iam_user_policy_attachment" "admin_policy_attachment" {
+  user       = aws_iam_user.aws_terraform_user.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+resource "aws_iam_access_key" "aws_terraform_user_access_key" {
+  user = aws_iam_user.aws_terraform_user.name
+}
+
+resource "aws_iam_role" "ec2_s3_access" {
+  name = "ec2-s3-access"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+
+# Output the access key and secret key
+output "awscliuser_access_key_id" {
+  value = aws_iam_access_key.aws_terraform_user_access_key.id
+}
+
+output "awscliuser_secret_access_key" {
+  value = aws_iam_access_key.aws_terraform_user_access_key.secret
+}
