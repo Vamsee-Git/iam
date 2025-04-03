385491940dc92345fd0234ea85ef8c3494ae33f4
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-03 18:05:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,6 +11,22 @@ resource "aws_iam_access_key" "aws_terraform_user_access_key" {
   user = aws_iam_user.aws_terraform_user.name
 }
 
+resource "aws_iam_role" "ec2_s3_access" {
+  name = "ec2-s3-access1"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
 resource "aws_iam_role_policy" "ec2_s3_access_policy" {
   name   = "EC2S3AccessPolicy"
   role   = aws_iam_role.ec2_s3_access1.id
@@ -27,11 +43,6 @@ resource "aws_iam_role_policy" "ec2_s3_access_policy" {
       }
     ]
   })
-}
-
-resource "aws_iam_role_policy_attachment" "ec2_full_access_attachment" {
-  role       = aws_iam_role.ec2_s3_access.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
 }
 
 # Output the access key and secret key
